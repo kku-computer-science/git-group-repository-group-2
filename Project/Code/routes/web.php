a50b34f23b9aba4569a59c7b64c1137106438871
653380213-5 <wangphai.j@kkumail.com>
2025-02-26 23:18:02 +0700

    fixed data for insert to database

--- Project/Code/routes/web.php
+++ Project/Code/routes/web.php
@@ -52,7 +52,8 @@ use App\Http\Controllers\ImageManagementController;
 |
 */
 
-
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
 
 
 /*Route::group(['middleware' => ['auth']], function() {
