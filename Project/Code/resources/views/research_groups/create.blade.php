fa8bdf5e2fc7b4f2a7b0a84b93e2eeb1d69ec579
“Siriyakorn348-2” <160942703+Siriyakorn348-2@users.noreply.github.com>
2025-02-26 21:13:02 +0700

    update

--- Project/Code/resources/views/research_groups/create.blade.php
+++ Project/Code/resources/views/research_groups/create.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     @if ($errors->any())
     <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <strong>{{ __('message.whoops') }}</strong> {{ __('message.problem_with_input') }}<br><br>
         <ul>
             @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
@@ -14,72 +14,75 @@
     @endif
     <div class="card" style="padding: 16px;">
         <div class="card-body">
-            <h4 class="card-title">{{ __('message.create_research_group')}}</h4>
-            <p class="card-description">{{ __('message.create_detail')}}</p>
+        <h4 class="card-title">{{ __('message.create_research_group') }}</h4>
+        <p class="card-description">{{ __('message.edit_research_group') }}</p>
+
             <form action="{{ route('researchGroups.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>ชื่อกลุ่มวิจัย (ภาษาไทย)</b></p>
+                    <p class="col-sm-3 "><b>{{ __('message.group_name_th') }}</b></p>
                     <div class="col-sm-8">
                         <input name="group_name_th" value="{{ old('group_name_th') }}" class="form-control"
-                            placeholder="ชื่อกลุ่มวิจัย (ภาษาไทย)">
+                            placeholder="{{ __('message.group_name_th') }}">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>ชื่อกลุ่มวิจัย (English)</b></p>
+                    <p class="col-sm-3 "><b>{{ __('message.group_name_en') }}</b></p>
                     <div class="col-sm-8">
                         <input name="group_name_en" value="{{ old('group_name_en') }}" class="form-control"
-                            placeholder="ชื่อกลุ่มวิจัย (English)">
+                            placeholder="{{ __('message.group_name_en') }}">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>คำอธิบายกลุ่มวิจัย (ภาษาไทย)</b></p>
+                    <p class="col-sm-3"><b>{{ __('message.group_desc_th') }}</b></p>
                     <div class="col-sm-8">
                         <textarea name="group_desc_th" value="{{ old('group_desc_th') }}" class="form-control"
                             style="height:90px"></textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>คำอธิบายกลุ่มวิจัย (English)</b></p>
+                    <p class="col-sm-3"><b>{{ __('message.group_desc_en') }}</b></p>
                     <div class="col-sm-8">
                         <textarea name="group_desc_en" value="{{ old('group_desc_en') }}" class="form-control"
                             style="height:90px"></textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>รายละเอียดกลุ่มวิจัย (ภาษาไทย)</b></p>
+                    <p class="col-sm-3"><b>{{ __('message.group_detail_th') }}</b></p>
                     <div class="col-sm-8">
                         <textarea name="group_detail_en" value="{{ old('group_detail_th') }}" class="form-control"
                             style="height:90px"></textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>รายละเอียดกลุ่มวิจัย (English)</b></p>
+                    <p class="col-sm-3"><b>{{ __('message.group_detail_en') }}</b></p>
                     <div class="col-sm-8">
                         <textarea name="group_detail_en" value="{{ old('group_detail_en') }}" class="form-control"
                             style="height:90px"></textarea>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>image</b></p>
+                    <p class="col-sm-3"><b>{{ __('message.image') }}</b></p>
                     <div class="col-sm-8">
                         <input type="file" name="group_image" class="form-control" value="{{ old('group_image') }}">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>หัวหน้ากลุ่มวิจัย</b></p>
+                    <p class="col-sm-3"><b>{{ __('message.head_group') }}</b></p>
                     <div class="col-sm-8">
                         <select id='head0' name="head">
                             @foreach($users as $user)
                             <option value="{{ $user->id }}">
-                                {{ $user->fname_th }} {{ $user->lname_th }}
+
+                          {{ $user->fname_th }} {{ $user->lname_th }}
+                          
                             </option>
                             @endforeach
                         </select>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <p class="col-sm-3 pt-4"><b>สมาชิกกลุ่มวิจัย</b></p>
+                    <p class="col-sm-3 pt-4"><b>{{ __('message.members') }}</b></p>
                     <div class="col-sm-8">
                         <table class="table" id="dynamicAddRemove">
                             <tr>
@@ -89,8 +92,8 @@
                         </table>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary upload mt-5">Submit</button>
-                <a class="btn btn-light mt-5" href="{{ route('researchGroups.index')}}"> Back</a>
+                <button type="submit" class="btn btn-primary upload mt-5">{{ __('message.submit') }}</button>
+                <a class="btn btn-light mt-5" href="{{ route('researchGroups.index')}}"> {{ __('message.back') }}</a>
                 </form>
         </div>
     </div>
