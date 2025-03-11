944cba782f032a7f4e24374a9203e731f2a1fe72
Assani Indraprasitdhi <assaniindraprasitdhi@MacBook-Ai-M3r.local>
2025-03-11 21:36:49 +0700

    Change column detail in manage highlight.

--- Project/Code/resources/views/highlight/edit.blade.php
+++ Project/Code/resources/views/highlight/edit.blade.php
@@ -15,29 +15,29 @@
 
     <div class="card" style="padding: 16px;">
         <div class="card-body">
-            <h4 class="card-title">{{ __('message.edit_highlight') }}</h4>
-            <p class="card-description">{{ __('message.fill_info') }}</p>
+            <h4 class="card-title">Edit Highlight</h4>
+
             
             <form action="{{ route('highlights.update', $highlight->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
 
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>{{ __('message.title') }}</b></p>
+                    <p class="col-sm-3"><b>Title : </b></p>
                     <div class="col-sm-8">
                         <input name="title" value="{{ old('title', $highlight->title) }}" class="form-control" placeholder="{{ __('message.title') }}">
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>{{ __('message.detail') }}</b></p>
+                    <p class="col-sm-3"><b>Detail : </b></p>
                     <div class="col-sm-8">
                         <textarea name="detail" class="form-control" style="height:90px">{{ old('detail', $highlight->detail) }}</textarea>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>{{ __('message.thumbnail') }}</b></p>
+                    <p class="col-sm-3"><b>Thumbnail : </b></p>
                     <div class="col-sm-8">
                         <input type="file" name="thumbnail" class="form-control">
                         @if($highlight->thumbnail)
@@ -47,14 +47,14 @@
                 </div>
 
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>{{ __('message.tags') }}</b></p>
+                    <p class="col-sm-3"><b>Tags : </b></p>
                     <div class="col-sm-8">
                         <input name="tags" value="{{ old('tags', implode(',', $highlight->tags->pluck('name')->toArray())) }}" class="form-control" placeholder="{{ __('message.tags') }}">
                     </div>
                 </div>
 
-                <button type="submit" class="btn btn-primary mt-5">{{ __('message.submit') }}</button>
-                <a class="btn btn-light mt-5" href="{{ route('highlights.index') }}">{{ __('message.back') }}</a>
+                <button type="submit" class="btn btn-primary mt-5">Submit</button>
+                <a class="btn btn-light mt-5" href="{{ route('highlights.index') }}">Back</a>
             </form>
         </div>
     </div>
