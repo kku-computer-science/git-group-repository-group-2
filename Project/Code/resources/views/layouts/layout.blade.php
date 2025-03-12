44be5cf9069b18c8dc2f5022bc743096f4de8ffc
“Siriyakorn348-2” <160942703+Siriyakorn348-2@users.noreply.github.com>
2025-03-12 13:32:47 +0700

    update

--- Project/Code/resources/views/layouts/layout.blade.php
+++ Project/Code/resources/views/layouts/layout.blade.php
@@ -49,6 +49,8 @@
 
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
     
 
 </head>
